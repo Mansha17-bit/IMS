f25fe73aac8f1b085d340f8e01a57bdb451f50ca
Mansha17-bit <mansha.LNU2@cognizant.com>
2025-05-08 15:50:19 +0530

    IAgentServices.cs

--- Interface/IAgentServices.cs
+++ Interface/IAgentServices.cs
@@ -1,6 +1,19 @@
-﻿namespace InsuranceManagementSystem.Interface
+using InsuranceManagementSystem.DTOs;
+using InsuranceManagementSystem.Models;
+
+namespace InsuranceManagementSystem.Services
 {
     public interface IAgentServices
     {
+        Task<IEnumerable<Agent>> GetAllAgentsAsync();
+        Task<Agent> GetAgentByIdAsync(int id);
+        Task<Agent> AddAgentAsync(Agent agent);
+        Task<Agent> UpdateAgentAsync(int id, Agent agent);
+        Task<bool> DeleteAgentAsync(int id);
+        Task<bool> AppointAgentToPolicyAsync(int policyId, int agentId);
+        Task UpdateAgentAsync(int id, object agent_Name);
+        Task<IEnumerable<Policy>> GetAssignedPoliciesByAgentIdAsync(int agentId);
+        
     }
 }
+
